e2238ba58df5e1fe08058641906465490f927ed0
dikshamehta <500061597@stu.upes.ac.in>
2021-01-02 08:36:13 +0530

    CPP-functional object

---
+++ CPP-language/functional_objects.cpp
@@ -0,0 +1,14 @@
+/*
+Functional Objects  :   def1 -> objects of any class that supports operator()
+                    :   def2 -> the object to which a function pointer refers
+
+NOTE:
+wherever a functional object is expected a function pointer will also work 
+
+TYPES:
+1. Binary functional objects    :   correspond to binary operators, plus
+2. Unary functional objects     :   corresponds to unary opertors, like logical_not, negation   
+
+<functional> is a header file which contains common functional objects 
+
+*/
